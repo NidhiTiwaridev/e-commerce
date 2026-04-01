d60bf1cdffda5629912d990d3cf90cbfdcb00235
Nidhi Tiwari <nidhi.tiwari.tech@gmail.com>
2026-04-01 15:50:25 +0530

    Add files via upload

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+ await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  res.json({ message: "User registered" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.json({ message: "User not found" });
+  } const isMatch = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!isMatch) {
+    return res.json({ message: "Wrong password" });
+  }
+
+  const token = jwt.sign(
+    { id: user._id },
+    "secret",
+    { expiresIn: "1d" }
+  );
+
+  res.json({
+    message: "Login success",
+    token
+     });
+});
+
+module.exports = router;
